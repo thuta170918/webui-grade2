339e2aee7b2899c08253278b533da54aa0fc3841
thuta170918 <thuta170918@gmail.com>
2026-04-23 17:06:49 +0900

    My first comment

---
+++ tata/main.js
@@ -0,0 +1,11 @@
+// const name = prompt("名前は？");
+// alert("こんにちは " + name +"さん!");
+
+
+const target = document.getElementById("target");
+console.log(target.textContent);
+
+target.textContent = "Boom Boom!";
+target.style.color = "purple";
+target.style.fontSize = "10px";
+target.style.backgroundColor = "yellow";
